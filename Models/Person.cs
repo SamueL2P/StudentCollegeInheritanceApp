199206089d3ec654f1a370c8b36f6ba65585184f
Samuel <padmadansamuel@gmail.com>
2024-07-18 16:36:54 +0530

    Add project files.

---
+++ Models/Person.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentCollegeInheritanceApp.Models
+{
+    internal class Person
+    {
+        public int Id { get; set; }
+        public string Address { get; set; }
+        public DateOnly Dob {  get; set; }
+
+        public Person()
+        {
+            
+        }
+        public Person(int id , string address , DateOnly dob)
+        {
+            Id = id ;   
+            Address = address ;
+            Dob = dob ;
+        }
+
+        public virtual string PrintDetails() {
+
+            return $"Person Id : {Id} \nPerson Address : {Address} \nPerson Date of Birth : {Dob}";
+            
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using StudentCollegeInheritanceApp.Models;
+
+namespace StudentCollegeInheritanceApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person(1 , "Thane" , new DateOnly(2002 , 11 , 22));
+            Student student = new Student(2 , "Mumbai" , new DateOnly(2002 , 10 , 3) , "Computer");
+            Professor professor = new Professor(3 , "Andheri" , new DateOnly(2002 , 4 , 10) , 40000);
+
+            PrintDetails(person);
+            PrintDetails(student);
+            PrintDetails(professor);
+         
+        }
+
+        static void PrintDetails(Person person)
+        {
+            Console.WriteLine($"======================={person.GetType().Name}========================");
+            Console.WriteLine(person.PrintDetails());
+        }
+    }
+}
